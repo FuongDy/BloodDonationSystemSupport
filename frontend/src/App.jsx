f3f8f8aae4c1fd1ec208dfa63ed5e4bdea7937b3
ntu254 <nttu254.vn@gmail.com>
2025-06-15 09:35:03 +0700

    #4. tạo Homepage, thêm feature section, cài util cho tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Button from './components/common/Button'
 import LoadingSpinner from './components/common/LoadingSpinner'
+import HomePage from './pages/HomePage'
 
 export default function App() {
   return (
     <>
-      <Button />
-      <LoadingSpinner />
+      <HomePage />
     </>
   )
 }
