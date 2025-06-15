41fed4f29d217e14464ed750c03ce2c6131fafdc
Kangnahyun23 <tankhang6a6@gmail.com>
2025-06-16 00:30:49 +0700

    Thêm Badge vào phần components/ui và import vào app.jsx để test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import Button from './components/common/Button'
 import LoadingSpinner from './components/common/LoadingSpinner'
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotfoundPage'
-import { Avatar, AvatarImage, AvatarFallback } from './components/ui/Avatar' // Modified import
+import { Avatar, AvatarImage, AvatarFallback } from './components/ui/Avatar'
+import { Badge } from './components/ui/Badge' // Thêm import cho Badge
 
 export default function App() {
   return (
@@ -12,6 +13,18 @@ export default function App() {
         <AvatarImage src="https://github.com/shadcn.png" alt="User Avatar" />
         <AvatarFallback>U</AvatarFallback>
       </Avatar>
+
+      {/* Thêm các Badge để hiển thị */}
+      <div className="flex space-x-2 p-4">
+        <Badge>Default</Badge>
+        <Badge variant="success">Success</Badge>
+        <Badge variant="warning">Warning</Badge>
+        <Badge variant="error">Error</Badge>
+        <Badge variant="info">Info</Badge>
+        <Badge variant="primary">Primary</Badge>
+        <Badge variant="outline">Outline</Badge>
+      </div>
+      
       {/* Bạn có thể thêm NotFoundPage hoặc các component khác ở đây nếu cần */}
       {/* <NotFoundPage /> */}
     </>
