d70bde1494eaa42bf4b370712b8ed943e1e95518
Kangnahyun23 <tankhang6a6@gmail.com>
2025-06-15 17:35:18 +0700

    #8. Tạo NotFoundPage và fix bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Button from './components/common/Button'
 import LoadingSpinner from './components/common/LoadingSpinner'
 import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotfoundPage'
+
 
 export default function App() {
   return (
     <>
-      <HomePage />
+     
+      <NotFoundPage/>
     </>
   )
 }
