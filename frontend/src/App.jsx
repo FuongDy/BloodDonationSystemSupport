f4107fc036a1e0a894212fa4c32dbd62698ff118
Kangnahyun23 <tankhang6a6@gmail.com>
2025-06-16 01:16:56 +0700

    #14 Thêm ForbiddenPage vào giao diện web và fix bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,20 @@ import NotFoundPage from './pages/NotfoundPage'
 import { Avatar, AvatarImage, AvatarFallback } from './components/ui/Avatar'
 import { Badge } from './components/ui/Badge'
 import Footer from './components/layouts/Footer'
-import Navbar from './components/layouts/Navbar' // Thêm import cho Navbar
+import Navbar from './components/layouts/Navbar'
+import ForbiddenPage from './pages/ForbiddenPage' // Import ForbiddenPage
 
 export default function App() {
   return (
     <>
-      <Navbar /> {/* Thêm Navbar vào đầu trang */}
-      <Avatar>
+      {/* <Navbar /> */}
+      {/* <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" alt="User Avatar" />
         <AvatarFallback>U</AvatarFallback>
-      </Avatar>
+      </Avatar> */}
 
       {/* Thêm các Badge để hiển thị */}
-      <div className="flex space-x-2 p-4">
+      {/* <div className="flex space-x-2 p-4">
         <Badge>Default</Badge>
         <Badge variant="success">Success</Badge>
         <Badge variant="warning">Warning</Badge>
@@ -26,9 +27,11 @@ export default function App() {
         <Badge variant="info">Info</Badge>
         <Badge variant="primary">Primary</Badge>
         <Badge variant="outline">Outline</Badge>
-      </div>
+      </div> */}
       
-      <Footer /> {/* Thêm Footer vào cuối trang */}
+      <ForbiddenPage /> {/* Hiển thị ForbiddenPage */}
+
+      {/* <Footer /> */}
     </>
   )
 }
