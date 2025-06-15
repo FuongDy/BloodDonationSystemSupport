cdf035cce650914d98696a06be3a1090d598698c
ntu254 <nttu254.vn@gmail.com>
2025-06-15 10:34:24 +0700

    #7. Tạo StartsSection

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import FeaturesSection from '../components/home/FeaturesSection';
+import StatsSection from '../components/home/StatsSection';
 
 const HomePage = () => {
     return (
         <div className="min-h-screen bg-white">
             <main className="pt-16">
                 <FeaturesSection />
+                <StatsSection />
             </main>
         </div>
     );
