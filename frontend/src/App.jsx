2e0a104830d5b0d3ef12b9ac804de04d4e1d28dd
Kangnahyun23 <tankhang6a6@gmail.com>
2025-06-16 00:21:24 +0700

    #9. Tạo Avatar trong components và fix bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,17 @@ import Button from './components/common/Button'
 import LoadingSpinner from './components/common/LoadingSpinner'
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotfoundPage'
-
+import { Avatar, AvatarImage, AvatarFallback } from './components/ui/Avatar' // Modified import
 
 export default function App() {
   return (
     <>
-     
-      <NotFoundPage/>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="User Avatar" />
+        <AvatarFallback>U</AvatarFallback>
+      </Avatar>
+      {/* Bạn có thể thêm NotFoundPage hoặc các component khác ở đây nếu cần */}
+      {/* <NotFoundPage /> */}
     </>
   )
 }
