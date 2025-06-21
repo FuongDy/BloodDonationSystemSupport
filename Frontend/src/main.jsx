bc094040997111b1584edfba60c257275f2b4865
Kangnahyun23 <tankhang6a6@gmail.com>
2025-06-21 10:36:42 +0700

    Fix bug tạo yêu cầu máu khẩn cấp

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -15,6 +15,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css'; // Import CSS của react-toastify
 
 // Hàm để khởi động MSW
 async function enableMocking() {
