86709164dc2ff9ef21a4408136ba97c5f6d21457
Kangnahyun23 <tankhang6a6@gmail.com>
2025-06-22 13:19:47 +0700

    nâng cấp  pagecontainer

--- frontend/src/components/layouts/PageContainer.jsx
+++ frontend/src/components/layouts/PageContainer.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
-import { Container } from '@mantine/core';
+import { cn } from '../../utils/cn';
 
-// Mantine version of PageContainer using Container component
 const PageContainer = ({
-    children,
-    size = "xl",
-    ...props
+  children,
+  className,
+  maxWidth = '7xl',
+  padding = true,
+  ...props
 }) => {
-    return (
-        <Container size={size} {...props}>
-            {children}
-        </Container>
-    );
+  const maxWidthClasses = {
+    sm: 'max-w-sm',
+    md: 'max-w-md',
+    lg: 'max-w-lg',
+    xl: 'max-w-xl',
+    '2xl': 'max-w-2xl',
+    '3xl': 'max-w-3xl',
+    '4xl': 'max-w-4xl',
+    '5xl': 'max-w-5xl',
+    '6xl': 'max-w-6xl',
+    '7xl': 'max-w-7xl',
+    full: 'max-w-full',
+  };
+
+  return (
+    <div
+      className={cn(
+        'mx-auto',
+        maxWidthClasses[maxWidth],
+        padding && 'px-4 sm:px-6 lg:px-8',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default PageContainer;
