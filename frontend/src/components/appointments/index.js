51c44247af83dc9ba04cf9467c6f1fc347a2fdf2
ntu254 <nttu254.vn@gmail.com>
2025-06-28 12:50:36 +0700

    Delete frontend directory

--- frontend/src/components/appointments/index.js
+++
@@ -1,5 +0,0 @@
-// src/components/appointments/index.js
-export { default as AppointmentsPageHeader } from './AppointmentsPageHeader';
-export { default as AppointmentCard } from './AppointmentCard';
-export { default as AppointmentsList } from './AppointmentsList';
-export { default as RescheduleModal } from './RescheduleModal';
